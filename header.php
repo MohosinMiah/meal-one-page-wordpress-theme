6287d7b50df366f36e67e3107125e83dd5f4be9e
mohosin16103308 <hamza1610330816@gmail.com>
2019-08-16 19:37:20 +0600

    complete mastering

---
+++ header.php
@@ -0,0 +1,29 @@
+
+<!doctype html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <title>Meal &mdash; Free Template for Restaurants by Free-Template.co</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Free Template by Free-Template.co"/>
+    <meta name="keywords" content="free template, free bootstrap, free website template"/>
+    <meta name="author" content="Free-Template.co"/>
+
+    <?php wp_head(); ?>
+
+
+
+    <!--
+    //////////////////////////////////////////////////////
+
+    Free TEMPLATE 
+    DESIGNED & DEVELOPED by free-template.co
+      
+    Website:    https://free-template.co
+    Email:      freetemplate.co@gmail.com
+    Twitter:    https://twitter.com/Free_Templateco
+    Facebook:   https://www.facebook.com/Free.Template.co/
+
+    //////////////////////////////////////////////////////
+    -->
+</head>
--- index.php
+++ index.php
@@ -1,35 +1,13 @@
+<?php  
+/**
+ * Template Name : Landing Page
+ */
 
-<!doctype html>
-<html lang="<?php language_attributes(); ?>">
-<head>
-    <title>Meal &mdash; Free Template for Restaurants by Free-Template.co</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Free Template by Free-Template.co"/>
-    <meta name="keywords" content="free template, free bootstrap, free website template"/>
-    <meta name="author" content="Free-Template.co"/>
-
-    <?php wp_head(); ?>
-
-
-
-    <!--
-    //////////////////////////////////////////////////////
-
-    Free TEMPLATE 
-    DESIGNED & DEVELOPED by free-template.co
-      
-    Website:    https://free-template.co
-    Email:      freetemplate.co@gmail.com
-    Twitter:    https://twitter.com/Free_Templateco
-    Facebook:   https://www.facebook.com/Free.Template.co/
-
-    //////////////////////////////////////////////////////
-    -->
-</head>
+?>
+<?php get_header(); ?>
 <!--<body class="bg-light">-->
 
-<body data-spy="scroll" data-target="#ftco-navbar-spy" data-offset="0">
+<body <?php body_class(); ?> data-spy="scroll" data-target="#ftco-navbar-spy" data-offset="0">
 
 <div class="site-wrap">
 
@@ -824,76 +802,6 @@
             </div>
         </div> <!-- .section -->
 
-        <div class="map-wrap" id="map" data-aos="fade"></div>
+    <div class="map-wrap" id="map" data-aos="fade"></div>
 
-
-        <footer class="ftco-footer">
-            <div class="container">
-
-                <div class="row">
-                    <div class="col-md-4 mb-5">
-                        <div class="footer-widget">
-                            <h3 class="mb-4">About Meal</h3>
-                            <p>The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild
-                                Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. </p>
-                            <!-- <input type="submit" class="btn btn-primary btn-outline-primary" value="Send Message"> -->
-                            <p><a href="https://free-template.co" target="_blank"
-                                  class="btn btn-primary btn-outline-primary">More Templates</a></p>
-                        </div>
-                    </div>
-                    <div class="col-md-4 mb-5">
-                        <div class="footer-widget">
-                            <h3 class="mb-4">Lunch Service</h3>
-                            <p>Booking from 12:00pm &mdash; 1:30pm</p>
-                            <h3 class="mb-4">Dinner Service</h3>
-                            <p>Everyday: <br> Booking from 6:00pm &mdash; 9:00pm</p>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4">
-                        <div class="footer-widget">
-                            <h3 class="mb-4">Follow Along</h3>
-                            <ul class="list-unstyled social">
-                                <li><a href="#"><span class="fa fa-tripadvisor"></span></a></li>
-                                <li><a href="#"><span class="fa fa-twitter"></span></a></li>
-                                <li><a href="#"><span class="fa fa-facebook"></span></a></li>
-                                <li><a href="#"><span class="fa fa-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                        <div class="footer-widget">
-                            <h3 class="mb-4">Newsletter</h3>
-                            <form action="#" class="ftco-footer-newsletter">
-                                <div class="form-group">
-                                    <button class="button"><span class="fa fa-envelope"></span></button>
-                                    <input type="email" class="form-control" placeholder="Enter Email">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="row pt-5">
-                    <div class="col-md-12 text-center">
-                        <p>&copy; Copyright 2018. All Rights Reserved. Designed &amp; Developed by <a
-                                href="https://free-template.co/">Free-Template.co</a></p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-
-    </div>
-</div>
-<!-- loader -->
-<div id="loader" class="show fullscreen">
-    <svg class="circular" width="48px" height="48px">
-        <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
-        <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
-                stroke="#ff7a5c"/>
-    </svg>
-</div>
-
-
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
